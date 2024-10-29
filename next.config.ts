eb5cd11fbeab5189c235defe499b8236267992d0
Marta Moros <marta.moros@bitbrain.com>
2024-10-29 12:59:56 +0100

    Disabling X Accel Buffering for streaming

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  headers: async () => {
+    return [
+      {
+        source: '/:path*{/}?',
+        headers: [
+          {
+            key: 'X-Accel-Buffering',
+            value: 'no',
+          },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
