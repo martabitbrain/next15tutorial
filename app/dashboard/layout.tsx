d389b4123af676de858676b14e95f2b2fe84ca3f
Marta Moros <marta.moros@bitbrain.com>
2024-10-31 09:12:35 +0100

    Chapter 13

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -11,8 +11,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           <SideNav />
         </div>
         <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
-          <h1>Dashboard Layout</h1>
-          <div>{children}</div>
+          {children}
         </div>
       </div>
     </>
